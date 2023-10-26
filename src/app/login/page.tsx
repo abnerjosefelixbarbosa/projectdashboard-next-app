fe33a8c18b30f950ceacf76b0df44d2f0805fc9d
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2023-10-26 17:03:50 -0300

    committed by Abner

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,7 +7,29 @@ export default function Login() {
       <header>
         <NavBar />
       </header>
-      <main></main>
+      <main>
+        <section className="center form-login-container">
+          <div className="form-login">
+            <div className="form-login-hearder"></div>
+            <div className="form-login-body">
+              <form>
+                <div>
+                  <label htmlFor="">Email:</label>
+                  <input type="text" />
+                </div>
+                <div>
+                  <label htmlFor="">Password:</label>
+                  <input type="text" />
+                </div>
+                <div>
+                  <button type="submit">Login</button>
+                </div>
+              </form>
+            </div>
+            <div className="form-login-fooder"></div>
+          </div>
+        </section>
+      </main>
       <footer>
         <Footer />
       </footer>

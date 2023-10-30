fc060bd0da293a0f085f5e7399169becb9303333
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2023-10-30 17:22:20 -0300

    committed by Abner

--- src/app/create-account/page.tsx
+++ src/app/create-account/page.tsx
@@ -5,16 +5,31 @@ export default function CreateAccount() {
         <div>
           <main>
             <section className="create-account-container">
-              <div className=""></div>
-              <div>
+              <div className="create-account-header"></div>
+              <div className="create-account-body">
                 <form>
-                  <div>
+                  <div className="create-account-form-controller">
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" id="name" />
+                  </div>
+                  <div className="create-account-form-controller">
                     <label htmlFor="email">Email:</label>
                     <input type="email" id="email" />
                   </div>
+                  <div className="create-account-form-controller">
+                    <label htmlFor="password">Password:</label>
+                    <input type="password" id="password" />
+                  </div>
+                  <div className="create-account-form-controller">
+                    <label htmlFor="password">Repeat Password:</label>
+                    <input type="password" id="password" />
+                  </div>
+                  <div className="center">
+                    <button className="create-button">Create</button>
+                  </div>
                 </form>
               </div>
-              <div className=""></div>
+              <div className="create-account-fooder"></div>
             </section>
           </main>
         </div>
